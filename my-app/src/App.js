6156064e0021c14957a611f95512bf8ca65513bf
Your Name <you@example.com>
2019-05-06 13:42:16 -0400

    props

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Ninjas from './ninjas';
 
 function App() {
   return (
     <div className="App">
       <h1>My First React App</h1>
       <p>Welcome :)</p>
+      <Ninjas name="Ryu" age="25" belt="black"/>
+      <br></br>
+      <Ninjas name="Yoshi" age="30" belt="red"/>
     </div>
   );
 }
